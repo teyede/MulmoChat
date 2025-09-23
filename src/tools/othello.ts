945ef4c7f865956e6ee9252180832943f8324360
snakajima <satoshi.nakajima@gmail.com>
2025-09-23 15:33:23 -0700

    Do not describe...

--- src/tools/othello.ts
+++ src/tools/othello.ts
@@ -146,7 +146,7 @@ const othello = async (
       ? "The game is over. Announce the game result."
       : isComputerTurn
         ? "The game state has been updated. It is your turn (you = AI assistant, computer). Make your move."
-        : "The game state has been updated. Tell the user to make a move. The user will tell you the move by specifying colum (A to H) and row (1 to 8)";
+        : "The game state has been updated. Tell the user to make a move. Do not describe the state of the game. The user is able to see it. The user will tell you the move by specifying colum (A to H) and row (1 to 8)";
 
     return {
       toolName,
